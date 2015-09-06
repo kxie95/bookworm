1ceffe4f1de3b6e5d7f9f6b741fed87ba4c7bd40
Karen Xie <kxie094@aucklanduni.ac.nz>
2015-09-06 15:54:11 +1200

    Added Address, Order and ShoppingCart domain classes.

--- src/main/java/kxie/uoa/bookshop/domain/ShoppingCart.java
+++ src/main/java/kxie/uoa/bookshop/domain/ShoppingCart.java
@@ -4,16 +4,12 @@ import java.util.HashMap;
 
 public class ShoppingCart {
 	private HashMap<Book, Integer> _booksInCart;
-	private double _totalPrice;
-	
-	public void addToCart(Book book, int quantity) {
-		_booksInCart.put(book, quantity);
-	}
 	
 	public HashMap<Book, Integer> getBooksInCart() {
 		return _booksInCart;
 	}
-	
-	public void getTotalPrice() {
+
+	public void set_booksInCart(HashMap<Book, Integer> _booksInCart) {
+		this._booksInCart = _booksInCart;
 	}
 }
