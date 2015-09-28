e26ec7cd4b39fc06e641ecaff5c4abed0cfda13f
kxie094@aucklanduni.ac.nz <xie-karen@hotmail.com>
2015-09-28 23:12:22 +1300

    Added servlet stuff.

--- src/main/java/kxie/uoa/bookshop/services/BookShopApplication.java
+++ src/main/java/kxie/uoa/bookshop/services/BookShopApplication.java
@@ -7,30 +7,27 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/services")
-public class BookShopApplication extends Application
-{
+public class BookShopApplication extends Application {
 	private Set<Object> singletons = new HashSet<Object>();
-	   private Set<Class<?>> classes = new HashSet<Class<?>>();
+	private Set<Class<?>> classes = new HashSet<Class<?>>();
 
-	   public BookShopApplication()
-	   {
-		  // Register the UserResource singleton to handle HTTP requests.
-		   UserResource resource = new UserResource();
-		   singletons.add(resource);
-	      // Register the ContextResolver class for JAXB.
-		   classes.add(UserResolver.class);
-	     
-	   }
+	public BookShopApplication() {
+		// Register the UserResource singleton to handle HTTP requests.
+		UserResource resource = new UserResource();
+		singletons.add(resource);
+		// Register the ContextResolver class for JAXB.
+		classes.add(UserResolver.class);
 
-	   @Override
-	   public Set<Object> getSingletons()
-	   {
-	      return singletons;
-	   }
-	   
-	   @Override
-	   public Set<Class<?>> getClasses()
-	   {
-	      return classes;
-	   }
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		return classes;
+	}
+
 }
